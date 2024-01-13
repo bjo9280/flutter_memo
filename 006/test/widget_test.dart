91ce7ec2134e7a1448332716c15af4a053618a5b
jongok ban <bjo9280@gmail.com>
2024-01-13 18:20:44 +0900

    006

--- 006/test/widget_test.dart
+++ 006/test/widget_test.dart
@@ -13,7 +13,7 @@ import 'package:memo_006/main.dart';
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(const MyApp());
+    await tester.pumpWidget(const MyMemoApp());
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
