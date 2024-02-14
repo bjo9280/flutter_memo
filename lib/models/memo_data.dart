b0028000eec01f3774744a402df5073357933e9f
jongok ban <bjo9280@gmail.com>
2024-02-14 13:15:57 +0900

    init

---
+++ lib/models/memo_data.dart
@@ -0,0 +1,21 @@
+class MemoData {
+  int? id;
+  String content;
+  final DateTime createAt;
+
+  MemoData({this.id, required this.content, required this.createAt});
+
+  @override
+  String toString() {
+    // TODO: implement toString
+    return 'content : $content, createAt : $createAt';
+  }
+
+  Map<String, dynamic> toMap() {
+    return {
+      'id': id,
+      'content': content,
+      'createAt': createAt.toIso8601String(),
+    };
+  }
+}
